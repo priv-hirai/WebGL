221b2287bd1744270328fafd973d81abe307b9a6
priv-hirai <hiraitakahiro0923@gmail.com>
2022-10-04 22:26:23 +0900

    時間差

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom'
-import { Suspense } from 'react'
+// import { Suspense } from 'react'
 import { Logo } from '@pmndrs/branding'
 import './styles.css'
 import { App } from './App'
 import Hello from './hello'
+import * as THREE from 'three'
+import { Suspense, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Preload, useCursor } from '@react-three/drei'
+import { Model, Fragments } from './Text'
 
 function Overlay() {
   return (
@@ -19,10 +24,36 @@ function Overlay() {
   )
 }
 
+function Scene() {
+  const vec = new THREE.Vector3()
+  const [clicked, setClicked] = useState(false)
+  const [hovered, setHovered] = useState(false)
+  useCursor(hovered)
+  useFrame((state) => {
+    state.camera.position.lerp(vec.set(clicked ? -10 : 0, clicked ? 10 : 0, 20), 0.1)
+    state.camera.lookAt(0, 0, 0)
+  })
+  setTimeout(function(){
+    setClicked(true)
+  },2000);
+  return (
+    <group>
+      <Fragments visible={clicked} />
+      <Model visible={!clicked} onClick={() => setClicked(true)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} />
+    </group>
+  )
+}
+
 ReactDOM.render(
   <>
     <Suspense fallback={null}>
-      <Hello />
+      <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 250 }}>
+        <ambientLight />
+        <Suspense fallback={null}>
+          <Scene />
+          <Preload all />
+        </Suspense>
+      </Canvas>
       <App />
     </Suspense>
     {/* <Overlay /> */}
