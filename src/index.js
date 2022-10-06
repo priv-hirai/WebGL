ee9c66b692d966a55612b492054011af58a27b55
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2022-10-06 11:09:06 +0900

    構成変更

--- src/index.js
+++ src/index.js
@@ -13,50 +13,52 @@ import { Model, Fragments } from './Text'
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
+      {/* <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
         pmnd.rs
         <br />
         dev collective
-      </a>
-      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>bad —</div>
+      </a> */}
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>
+        My pets
+      </div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>10/17/2021</div>
     </div>
   )
 }
 
-function Scene() {
-  const vec = new THREE.Vector3()
-  const [clicked, setClicked] = useState(false)
-  const [hovered, setHovered] = useState(false)
-  useCursor(hovered)
-  useFrame((state) => {
-    state.camera.position.lerp(vec.set(clicked ? -10 : 0, clicked ? 10 : 0, 20), 0.1)
-    state.camera.lookAt(0, 0, 0)
-  })
-  setTimeout(function(){
-    setClicked(true)
-  },2000);
-  return (
-    <group>
-      <Fragments visible={clicked} />
-      <Model visible={!clicked} onClick={() => setClicked(true)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} />
-    </group>
-  )
-}
+// function Scene() {
+//   const vec = new THREE.Vector3()
+//   const [clicked, setClicked] = useState(false)
+//   const [hovered, setHovered] = useState(false)
+//   useCursor(hovered)
+//   useFrame((state) => {
+//     state.camera.position.lerp(vec.set(clicked ? -10 : 0, clicked ? 10 : 0, 20), 0.1)
+//     state.camera.lookAt(0, 0, 0)
+//   })
+//   setTimeout(function(){
+//     setClicked(true)
+//   },2000);
+//   return (
+//     <group>
+//       <Fragments visible={clicked} />
+//       <Model visible={!clicked} onClick={() => setClicked(true)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} />
+//     </group>
+//   )
+// }
 
 ReactDOM.render(
   <>
     <Suspense fallback={null}>
-      <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 250 }}>
+      {/* <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 250 }}>
         <ambientLight />
         <Suspense fallback={null}>
           <Scene />
           <Preload all />
         </Suspense>
-      </Canvas>
+      </Canvas> */}
       <App />
     </Suspense>
-    {/* <Overlay /> */}
+    <Overlay />
     {/* <Logo style={{ position: 'absolute', bottom: 40, left: 40, width: 30 }} /> */}
   </>,
   document.getElementById('root')
