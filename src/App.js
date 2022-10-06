83312cc33a9eaf054109019e5d3b25692438ae61
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2022-10-06 12:59:55 +0900

    ビルド時に画像出るように設定

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function Items({ w = 0.7, gap = 0.15 }) {
     <ScrollControls horizontal damping={10} pages={(width - xW + urls.length * xW) / width}>
       <Minimap />
       <Scroll>
-        {urls.map((url, i) => <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={url} />) /* prettier-ignore */}
+        {urls.map((url, i) => <Item key={i} index={i} position={[i * xW, 0, 0]} scale={[w, 4, 1]} url={"/build/"+url} />) /* prettier-ignore */}
       </Scroll>
     </ScrollControls>
   )
