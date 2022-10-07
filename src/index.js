62f1887207892889ef1b0d4909a3169b3c88713c
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2022-10-07 10:40:27 +0900

    sass導入

--- src/index.js
+++ src/index.js
@@ -1,24 +1,14 @@
 import ReactDOM from 'react-dom'
-// import { Suspense } from 'react'
+import { Suspense } from 'react'
 import { Logo } from '@pmndrs/branding'
 import './styles.css'
+import './assets/style.scss'
 import { App } from './App'
-import Hello from './hello'
-import * as THREE from 'three'
-import { Suspense, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Preload, useCursor } from '@react-three/drei'
-import { Model, Fragments } from './Text'
 
 function Overlay() {
   return (
     <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}>
-      {/* <a href="https://pmnd.rs/" style={{ position: 'absolute', bottom: 40, left: 90, fontSize: '13px' }}>
-        pmnd.rs
-        <br />
-        dev collective
-      </a> */}
-      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>
+      <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }} className="test">
         My pets
       </div>
       <div style={{ position: 'absolute', bottom: 40, right: 40, fontSize: '13px' }}>10/17/2021</div>
@@ -26,36 +16,9 @@ function Overlay() {
   )
 }
 
-// function Scene() {
-//   const vec = new THREE.Vector3()
-//   const [clicked, setClicked] = useState(false)
-//   const [hovered, setHovered] = useState(false)
-//   useCursor(hovered)
-//   useFrame((state) => {
-//     state.camera.position.lerp(vec.set(clicked ? -10 : 0, clicked ? 10 : 0, 20), 0.1)
-//     state.camera.lookAt(0, 0, 0)
-//   })
-//   setTimeout(function(){
-//     setClicked(true)
-//   },2000);
-//   return (
-//     <group>
-//       <Fragments visible={clicked} />
-//       <Model visible={!clicked} onClick={() => setClicked(true)} onPointerOver={() => setHovered(true)} onPointerOut={() => setHovered(false)} />
-//     </group>
-//   )
-// }
-
 ReactDOM.render(
   <>
     <Suspense fallback={null}>
-      {/* <Canvas dpr={[1, 2]} orthographic camera={{ zoom: 250 }}>
-        <ambientLight />
-        <Suspense fallback={null}>
-          <Scene />
-          <Preload all />
-        </Suspense>
-      </Canvas> */}
       <App />
     </Suspense>
     <Overlay />
