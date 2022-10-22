182905b9edab2c32c249a48bf9e444c78c5dc4db
priv-hirai <hiraitakahiro0923@gmail.com>
2022-10-22 15:10:42 +0900

    ヘッダーの動き実装

--- src/compenents/header.js
+++ src/compenents/header.js
@@ -1,6 +1,5 @@
+import React, { useState } from 'react';
 import '../assets/scss/header.scss'
-
-
 
 
 export function Header() {
@@ -12,7 +11,36 @@ export function Header() {
     let month = now.getMonth() + 1;
     // 日数取得
     let day = now.getDate();
-    return (
+
+      // 768pxにしたときにheader-bar_activeクラスを削除する
+  function reportWindowSize() {
+    if(window.innerWidth >= 768){
+      document.getElementById("header-bar").classList.remove("header-bar_active");
+    } else if(window.innerWidth <= 767 && isExpand === true) {
+        document.getElementById("header-bar").classList.add("header-bar_active");
+    }
+  }
+  window.onresize = reportWindowSize;
+
+  const [isExpand, setIsExpand] = useState(false);
+
+  const expandMenu = () => {
+        // 展開フラグの真偽値を逆転させる
+        setIsExpand(!isExpand);
+
+        // ハンバーガーメニューボタンの変更
+        document.getElementById("header-hamburgerMenu").classList.toggle("header-hamburgerMenu_active");
+        // ナビゲーション開閉
+        document.getElementById("header-nav").classList.toggle("header-navMenu_active");
+        // ナビゲーション開閉
+        document.getElementById("header-nav_overlay").classList.toggle("header-overlay_active");
+
+        if(window.innerWidth <= 768){
+          document.getElementById("header-bar").classList.toggle("header-bar_active");
+        }
+  };
+
+  return (
             <header id="header" className="fixed top-0 w-full z-50">
             <div className="header-bar" id="header-bar">
               {/* 日付 */}
@@ -23,18 +51,18 @@ export function Header() {
                 </p>
               </div>
             </div>
-            <div id="header-hamburgerMenu" className="header-hamburgerMenu">
+            <div id="header-hamburgerMenu" className="header-hamburgerMenu" onClick={expandMenu}>
               <span></span>
             </div>
             <nav id="header-nav" className="header-nav">
               <div className="header-navMenu">
                 <ul className="header-navMenu-list">
-                  <li className="header-navMenu-item">
+                  <li className="header-navMenu-item" onClick={expandMenu}>
                     <a href="http://hirai.website/react/" target="_blank" rel="noreferrer"><span>Top</span></a>
                   </li>
                 </ul>
               </div>
-              <div id="header-nav_overlay" className="header-overlay"></div>
+              <div id="header-nav_overlay" className="header-overlay" onClick={expandMenu}></div>
             </nav>
           </header>
     )
