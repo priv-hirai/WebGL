4933634014e4950945de6261851a3c845edc16bc
priv-hirai <hiraitakahiro0923@gmail.com>
2022-10-22 15:00:10 +0900

    ヘッダーの形作成。動きは後から実装する。

--- src/compenents/header.js
+++ src/compenents/header.js
@@ -1,21 +1,29 @@
-
+import '../assets/scss/header.scss'
 
 
 
 
 export function Header() {
+    // 日付型取得
+    let now = new Date();
+    // 年数取得
+    let year = now.getFullYear();
+    // 月数取得
+    let month = now.getMonth() + 1;
+    // 日数取得
+    let day = now.getDate();
     return (
             <header id="header" className="fixed top-0 w-full z-50">
-            <div className=" bg-white h-[4.375rem] opacity-[0.95] w-full" id="header-bar">
+            <div className="header-bar" id="header-bar">
               {/* 日付 */}
               <div className="mx-auto max-w-[78.125rem] px-[1.5625rem] w-full">
-                <p className="flex items-center h-[4.375rem] pl-[.75rem]">
-                  <span className="header-year text-[1.5625rem]">2022.</span>
-                  <span className="header-month_day text-[3rem] ml-[.375rem]">10.22</span>
+                <p className="header-date">
+                  <span className="header-year">{year}.</span>
+                  <span className="header-month_day">{month}.{day}</span>
                 </p>
               </div>
             </div>
-            <div id="header-hamburgerMenu" className="header-hamburgerMenu fixed right-0 top-0 w-[4.375rem] h-[4.375rem] bg-[#004c71] z-50">
+            <div id="header-hamburgerMenu" className="header-hamburgerMenu">
               <span></span>
             </div>
             <nav id="header-nav" className="header-nav">
